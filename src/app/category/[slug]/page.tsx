8ead598afd947eef88fa10fcbc676454fd03a111
Houn Chanminea <hc2.minea@gmail.com>
2025-03-09 10:43:57 +0700

    style: add rounded border to titl

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -79,7 +79,7 @@ export default function Page() {
 
           {/* Right Side Grid (Smaller Cards) */}
           <div className="flex-1 pl-2 overflow-auto">
-            <header className='p-4 bg-blue-500 text-white'>
+            <header className='p-4 bg-blue-500 text-white rounded-bl'>
               <Link href="/">Home</Link> / {decodeURIComponent(slug)}
             </header>
             <ul className="grid grid-cols-4 gap-x-4 gap-y-4 mt-2">
