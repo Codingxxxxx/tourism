7fef93eea837c48ef23e63e4f32cbbe6c4d25a3f
codingxxxxx <hc2.minea@gmail.com>
2025-05-29 22:01:03 +0700

    fix(admin/dashboard): convert page to client side

--- src/server/actions/dashboard.tsx
+++ src/server/actions/dashboard.tsx
@@ -1,14 +1,19 @@
 'use server';
 
 import { DashboardStatistic } from '@/shared/types/dto';
-import { HttpClient } from '../libs/httpClient';
+import { buildResponse, HttpClient } from '../libs/httpClient';
 import { ApiEndpont } from '../const/api';
+import { ServerResponse } from '@/shared/types/serverActions';
 
-export async function getDashboardStatistic(): Promise<DashboardStatistic> {
-  const { data } = await HttpClient.request({
+export async function getDashboardStatistic(): Promise<ServerResponse<DashboardStatistic>> {
+  const { data, isOk, unauthorized } = await HttpClient.request({
     method: 'GET',
     url: ApiEndpont.DASHBOARD_STATISTIC
   });
 
-  return data;
+  return buildResponse({
+    isUnauthorized: unauthorized,
+    success: isOk,
+    data
+  })
 }
